07758d56d5a06af22c01cb79da40a47f804edf5a
Kevin <skryfall@gmail.com>
2020-03-19 11:18:46 -0600

    Bugfix

--- Server/Source/Auxiliar.cs
+++ Server/Source/Auxiliar.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Server.Source
 {
@@ -72,5 +75,6 @@ namespace Server.Source
 
             return foundDate;
         }
+
     }
 }
