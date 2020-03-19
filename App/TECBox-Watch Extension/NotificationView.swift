3a776088367d2e7a5da1fa7bcc97859c88c6010d
Alejandro <josealejandroibarra@hotmail.com>
2020-03-18 19:30:26 -0600

    Now with pandas. 🐼

---
+++ App/TECBox-Watch Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  TECBox-Watch Extension
+//
+//  Created by Alejandro Ibarra on 3/16/20.
+//  Copyright © 2020 Schlafenhase. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
