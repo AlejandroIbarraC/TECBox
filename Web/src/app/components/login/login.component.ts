cdfadaf6f165c71952454fbd0e09858e6fb231a1
Alejandro <josealejandroibarra@hotmail.com>
2020-05-10 12:28:51 -0600

    Now with organized methods

--- Web/src/app/components/login/login.component.ts
+++ Web/src/app/components/login/login.component.ts
@@ -20,7 +20,8 @@ export class LoginComponent implements OnInit {
   }
 
   logInTeam(email: string, password: string) {
-    // ADD SERVER VERIFICATION HERE
+    // CONNECT TO SERVER AND VERIFY IF TEAM MEMBER EXISTS
+
     if (this.userType == "admin") {
       this.router.navigateByUrl('/admin');
       localStorage.setItem('userType', 'admin')
@@ -33,7 +34,18 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  logInUser(email: string, password: string) {
+  logInUser() {
+    // Get information and store it in a const
+    const email = (document.getElementById('userEmail') as HTMLInputElement).value;
+    const password = (document.getElementById('userPassword') as HTMLInputElement).value;
+
+    // Empty all entries
+    (document.getElementById('userEmail') as HTMLInputElement).value = '';
+    (document.getElementById('userPassword') as HTMLInputElement).value = '';
+
+    // CONNECT TO SERVER AND VERIFY IF USER EXISTS
+
+    // Log in using Firebase (ONLY IF USER EXISTS IN SERVER)
     this.authService.SignIn(email, password);
   }
 
@@ -41,57 +53,31 @@ export class LoginComponent implements OnInit {
     this.userType = value;
   }
 
-  register(email: string, password: string) {
-    this.authService.SignUp(email, password)
+  register() {
+    // Get information and store it in a const
+    const name = (document.getElementById('r1') as HTMLInputElement).value;
+    const id = (document.getElementById('r2') as HTMLInputElement).value;
+    const email = (document.getElementById('r3') as HTMLInputElement).value;
+    const password = (document.getElementById('r4') as HTMLInputElement).value;
+    const phone = (document.getElementById('r5') as HTMLInputElement).value;
+    const address = (document.getElementById('r6') as HTMLInputElement).value;
+    const province = (document.getElementById('r7') as HTMLInputElement).value;
+    const city = (document.getElementById('r8') as HTMLInputElement).value;
+
+    // Register user in Firebase
+    this.authService.SignUp(email, password);
+
+    // Empty all entries
+    (document.getElementById('r1') as HTMLInputElement).value = '';
+    (document.getElementById('r2') as HTMLInputElement).value = '';
+    (document.getElementById('r3') as HTMLInputElement).value = '';
+    (document.getElementById('r4') as HTMLInputElement).value = '';
+    (document.getElementById('r5') as HTMLInputElement).value = '';
+    (document.getElementById('r6') as HTMLInputElement).value = '';
+    (document.getElementById('r7') as HTMLInputElement).value = '';
+    (document.getElementById('r8') as HTMLInputElement).value = '';
+
+    // CONNECT TO SERVER AND POST NEW REGISTERED USER DATA
   }
 
-//Methods created for get the data in the entries {Jesus Sandoval 10/05/2020}
-  getsingleAdminData(){
-
-    //Getting the information and stored it in a const
-    const email = (document.getElementById('tres') as HTMLInputElement).value;
-    const password = (document.getElementById('cuatro') as HTMLInputElement).value;
-
-    //This part will empty all the entries after the method is called
-    (document.getElementById('tres') as HTMLInputElement).value = '';
-    (document.getElementById('cuatro') as HTMLInputElement).value = '';
-
-  }
-
-  getsingleMemberData(){
-
-    //Getting the information and stored it in a const
-    const email = (document.getElementById('uno') as HTMLInputElement).value;
-    const password = (document.getElementById('dos') as HTMLInputElement).value;
-
-    //This part will empty all the entries after the method is called
-    (document.getElementById('uno') as HTMLInputElement).value = '';
-    (document.getElementById('dos') as HTMLInputElement).value = '';
-
-
-  }
-
-  getsingleRegisterData(){
-
-    //Getting the information and stored it in a const
-    const name = (document.getElementById('cinco') as HTMLInputElement).value;
-    const id = (document.getElementById('seis') as HTMLInputElement).value;
-    const email = (document.getElementById('siete') as HTMLInputElement).value;
-    const password = (document.getElementById('ocho') as HTMLInputElement).value;
-    const phone = (document.getElementById('nueve') as HTMLInputElement).value;
-    const address = (document.getElementById('diez') as HTMLInputElement).value;
-    const province = (document.getElementById('once') as HTMLInputElement).value;
-    const city = (document.getElementById('doce') as HTMLInputElement).value;
-
-    //This part will empty all the entries after the method is called
-    (document.getElementById('cinco') as HTMLInputElement).value = '';
-    (document.getElementById('seis') as HTMLInputElement).value = '';
-    (document.getElementById('siete') as HTMLInputElement).value = '';
-    (document.getElementById('ocho') as HTMLInputElement).value = '';
-    (document.getElementById('nueve') as HTMLInputElement).value = '';
-    (document.getElementById('diez') as HTMLInputElement).value = '';
-    (document.getElementById('once') as HTMLInputElement).value = '';
-    (document.getElementById('doce') as HTMLInputElement).value = '';
-
-  }
 }
